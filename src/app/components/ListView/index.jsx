47fdaf1e73f38bbdca6383ba003aedd35fa06426
AbhishekSatyam13 <abhishek.satyam@antwalk.com>
2024-07-14 16:17:20 +0530

    Add trip feature

--- src/app/components/ListView/index.jsx
+++ src/app/components/ListView/index.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ListViewWrapper } from "./style";
-import { Button, Table } from "antd";
+import { Button, Table, Modal, Form, Input, Row, Col, Select, message } from "antd";
 import record from "../../../mock/mockData.json";
+import { v4 as uuidv4 } from "uuid";
+
+const { Option } = Select;
 
 const columns = [
   {
@@ -57,16 +60,103 @@ const columns = [
 ];
 
 const ListView = () => {
-  console.log("record here", record);
+  //...................Hooks...................//
+
+  const [isModal, setModal] = useState(false);
+  const [mainRecord, setMainRecord] = useState([]);
+  const [payload, setPayload] = useState({
+    tripId: uuidv4(),
+    source: "",
+    phoneNumber: "",
+    transporter: '',
+    dest: '',
+  });
+
+  useEffect(() => {
+    if (window.localStorage.getItem("tableRecord")) {
+      setMainRecord(JSON.parse(localStorage.getItem("tableRecord")));
+    } else {
+      setMainRecord(record.data);
+      localStorage.setItem("tableRecord", JSON.stringify(record.data));
+    }
+  }, [window]);
+
+  //..........................................//
+
+  const handleChange = (fieldName, event) => {
+    setPayload((prevState) => ({
+      ...prevState,
+      [fieldName]: event.target?.value ?? event
+    }));
+  };
+
+  const handleSubmit = () => {
+    if(Object.values(payload).some(item => !item)){
+        message.info("All fields are mandatory");
+        return;
+    }
+    const existingData = JSON.parse(localStorage.getItem('tableRecord'));
+    existingData.unshift(payload);
+    setMainRecord(existingData);
+    message.success("Successfully Added trip!!");
+    localStorage.setItem('tableRecord',JSON.stringify(existingData));
+  }
+
   return (
     <ListViewWrapper>
       <header>
         <h3>Trip List</h3>
         <div>
-          <Button type="primary">Add Trip</Button>
+          <Button type="primary" onClick={() => setModal(true)}>
+            Add Trip
+          </Button>
         </div>
       </header>
-      <Table dataSource={record.data} columns={columns} rowKey="tripId" />
+      <Table
+        dataSource={mainRecord}
+        columns={columns}
+        rowKey="tripId"
+        loading={mainRecord.length < 1}
+      />
+      <Modal
+        title="Add Trip"
+        open={isModal}
+        width={900}
+        footer={[
+          <Button onClick={() => setModal(false)} key='cancel'>Cancel</Button>,
+          <Button type="primary" onClick={handleSubmit} key='submit'>Add Trip</Button>,
+        ]}
+        onCancel={() => setModal(false)}
+      >
+        <Form name="Add trip">
+          <Row gutter={24}>
+            <Col span={12}>
+              <Form.Item label="Trip Id" required>
+                <Input value={payload.tripId} disabled />
+              </Form.Item>
+              <Form.Item label="Source" required>
+                <Input onChange={(event) => handleChange("source", event)} />
+              </Form.Item>
+              <Form.Item label="Ph Number" required>
+                <Input onChange={(event) => handleChange("phoneNumber", event)}/>
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item label="Transporter" required>
+                <Select onChange={(event) => handleChange("transporter", event)}>
+                    <Option value="FedEx" key="FedEx">FedEx</Option>
+                    <Option value="Bluedart" key="Bluedart">Bluedart</Option>
+                    <Option value="DHL" key="DHL">DHL</Option>
+                    <Option value="Gati" key="Gati">Gati</Option>
+                </Select>
+              </Form.Item>
+              <Form.Item label="Destination" required>
+                <Input onChange={(event) => handleChange("dest", event)}/>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </Modal>
     </ListViewWrapper>
   );
 };
